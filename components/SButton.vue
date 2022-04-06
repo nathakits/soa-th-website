c008e003e5d139327f10c0937ccc9e8f37c64149
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2022-04-06 23:46:07 +0700

    add button component

---
+++ components/SButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="rounded-lg p-4 text-white hover:bg-blue-600" :class="bgColor">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    bgColor: {
+      type: String,
+      default: 'bg-blue-700',
+    },
+  },
+}
+</script>
+
+<style></style>
